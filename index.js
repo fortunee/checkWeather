b2fb70eee30ddda4d6073277709302374a6f1f22
Fortune Ekeruo <legalmody@gmail.com>
2019-03-07 21:17:35 +0100

    Define function for getting weather by zip code and use environment variable for  openweathermap API key

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+
+const APPID = process.env.APP_ID;
+
+const getWeatherByZipCode = async (zip) => {
+  const endPoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${APPID}`;
+  try {
+    return await axios.get(endPoint);
+  } catch (error) {
+    console.error(error)
+  }
+}
