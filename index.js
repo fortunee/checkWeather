2cd1a600e28767ce9b63d49f12ae6f523aa7fd8f
Fortune Ekeruo <legalmody@gmail.com>
2019-03-08 10:33:58 +0100

    Add a checkweather function that takes the location and zipcode to get the weather for a given location

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ dotenv.config()
 const APPID = process.env.APP_ID;
 
 const getWeatherByZipCode = async (zip) => {
-  const endPoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${APPID}`;
-  try {
-    return await axios.get(endPoint);
-  } catch (error) {
-    console.error(error)
-  }
+    const endPoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=${APPID}`;
+    try {
+        return await axios.get(endPoint);
+    } catch (error) {
+        console.error(error)
+    }
 }
+
+const checkWeather = async ([locationName, locationZipCode]) => {
+    const currentTime = new Date().toISOString();
+    const currentWeather = await getWeatherByZipCode(locationZipCode);
+
+    if (currentWeather.data) {
+        const weatherData = currentWeather.data.weather;
+        console.log(`${currentTime}: There's a ${weatherData[weatherData.length - 1].description} in ${locationName}`)
+    }
+}
+
+checkWeather(['NYC', 10001])
