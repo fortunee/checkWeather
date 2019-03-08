2bd24a217c9f32f196701fa531ebf4da41ac2358
Fortune Ekeruo <legalmody@gmail.com>
2019-03-08 10:51:31 +0100

    Follow invisible tech coding guidelines

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from 'axios'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const APPID = process.env.APP_ID;
-const URL = 'http://api.openweathermap.org/data/2.5/weather?zip';
+const APPID = process.env.APP_ID
+const URL = 'http://api.openweathermap.org/data/2.5/weather?zip='
 
 const getWeatherByZipCode = async (zip) => {
-    const endPoint = `${URL}=${zip},us&APPID=${APPID}`;
-    try {
-        return await axios.get(endPoint);
-    } catch (error) {
-        console.error(error);
-    }
+  const endPoint = `${URL}${zip},us&APPID=${APPID}`
+  try {
+    return await axios.get(endPoint)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const checkWeather = async ([locationName, locationZipCode]) => {
-    const currentTime = new Date().toISOString();
-    const currentWeather = await getWeatherByZipCode(locationZipCode);
+  const currentTime = new Date().toISOString()
+  const currentWeather = await getWeatherByZipCode(locationZipCode)
 
-    if (currentWeather.data) {
-        const weatherData = currentWeather.data.weather;
-        console.log(`${currentTime}: There's a ${weatherData[weatherData.length - 1].description} in ${locationName}`);
-    }
+  if (currentWeather.data) {
+    const weatherData = currentWeather.data.weather
+    console.log(`${currentTime}: There's a ${weatherData[weatherData.length - 1].description} in ${locationName}`)
+  }
 }
 
-checkWeather(['NYC', 10001]);
+checkWeather(['NYC', 10001])
